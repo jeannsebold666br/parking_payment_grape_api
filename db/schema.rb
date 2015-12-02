bf8c966a77ae12024304766558bd4c6b69b4c15c
Jorge Henrique de S. Rocha <jhrocha.sousa@gmail.com>
2015-12-01 22:23:45 -0300

    Add migration - Price column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151130233133) do
+ActiveRecord::Schema.define(version: 20151202012213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20151130233133) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean  "paid"
+    t.float    "price"
   end
 
   add_index "tickets", ["user_id"], name: "index_tickets_on_user_id", using: :btree
