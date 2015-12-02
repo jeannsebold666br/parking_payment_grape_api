ccfb0421bf6ba86dff6ad1b448a71daff5808eb7
Jorge Henrique de S. Rocha <jhrocha.sousa@gmail.com>
2015-12-01 22:26:41 -0300

    Price added on ticket

--- app/api/parking_payment/v1/tickets.rb
+++ app/api/parking_payment/v1/tickets.rb
@@ -20,8 +20,9 @@ class ParkingPayment::V1::Tickets < Grape::API
 
   end
 
-  desc 'Create a ticket of vehicle'
   resource :ticket do
+
+    desc 'Create a ticket of vehicle'
     params do
       requires :vehicle_id, type: Integer
     end
@@ -30,12 +31,15 @@ class ParkingPayment::V1::Tickets < Grape::API
       vehicles_has_pending? params[:vehicle_id]
       params[:user_id]= @current_user.id
       params[:paid]= false
+      params[:price]= 0.0
       Ticket.create params
     end
 
+    desc 'Pay a ticket'
     params do
       requires :ticket_id, type: Integer
       requires :vehicle_id, type: Integer
+      requires :price, type: Float
     end
     post :pay do
       vehicle= vehicle_exists? params[:vehicle_id]
@@ -43,7 +47,7 @@ class ParkingPayment::V1::Tickets < Grape::API
       ticket= Ticket.find_by id: params[:ticket_id], paid: false
       error! "There is a ticket to be paid for the vehicle with id #{params[:vehicle_id]}", 500 unless ticket
 
-      ticket.update paid: true
+      ticket.update paid: true, price: params[:price]
       ticket
     end
 
